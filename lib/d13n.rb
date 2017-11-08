645900fe3337dd126c4c9f203f355c353b0a450b
Ben Wu <ben.wu@laxino.com>
2017-11-08 23:26:32 +0800

    add service features

--- lib/d13n.rb
+++ lib/d13n.rb
@@ -1,10 +1,35 @@
 require 'd13n/version'
 require 'logger'
 module D13n
-  def self.logger
-    @logger = ::Logger.new(STDOUT)
-  end
+  class D13nError < StandardError;end
+  
+  class << self
+    # def config
+    #   @config ||= D13n::Configuration::Manager.new
+    # end
 
-  def self.logger=(logger)
+    # def logger
+    #   @logger ||= D13n::Logger::StartupLogger.instance
+    # end
+
+    # def logger=(log)
+    #   @logger = log
+    # end
+
+    # def opt_state
+    #   D13n::Api::OperationState.opt_get
+    # end
+
+    def threaded
+      Thread.current[:d13n] ||= {}
+    end
+
+    def service
+      threaded[:service] ||= nil
+    end
+
+    def service=(srv)
+      threaded[:service] = srv
+    end
   end
 end
