0db481334181bd0e01d2690879dbbaa1c5971c70
Ben Wu <ben.wu@laxino.com>
2017-11-20 20:34:14 +0800

    update application class methods to using D13n methods

--- lib/d13n/application/class_methods.rb
+++ lib/d13n/application/class_methods.rb
@@ -1,33 +1,35 @@
-module D13n
-  class Application
-    module ClassMethods
-      def config
-        @config ||= D13n.config
-      end
+module D13n::Application
+  module ClassMethods
+    def config
+      @config ||= D13n.config
+    end
 
-      def config=(cfg)
-        @config = cfg
-      end
+    def config=(cfg)
+      @config = cfg
+    end
 
-      def logger
-        @logger ||= D13n.logger
-      end
+    def logger
+      @logger ||= D13n.logger
+    end
 
-      def logger=(log)
-        @logger = log
-      end
+    def logger=(log)
+      @logger = log
+    end
 
-      def default_source
-        D13n::Configuration::DefaultSource.defaults
-      end
+    def default_source
+      D13n::Configuration::DefaultSource.defaults
+    end
 
-      def default_source=(default_config)
-        D13n::Configuration::DefaultSource.defaults = default_config
-      end
+    def default_source=(default_config)
+      D13n::Configuration::DefaultSource.defaults = default_config
+    end
 
-      def opt_state
-        D13n::Api::OperationState.opt_get
-      end
+    def opt_state
+      D13n::Api::OperationState.opt_get
+    end
+
+    def service
+      Service
     end
   end
 end
