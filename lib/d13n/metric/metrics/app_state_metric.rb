22118983e1443f2b531f2fe38b8fcd350fc0f0c0
Ben Wu <ben.wu@laxino.com>
2018-04-10 10:46:57 +0800

    add app state metric and exception spec

--- lib/d13n/metric/metrics/app_state_metric.rb
+++ lib/d13n/metric/metrics/app_state_metric.rb
@@ -1,4 +1,99 @@
 module D13n::Metric
   class AppStateMetric < Base
+    module Namespace
+      def gem_metric_tags(gem_info)
+        tags = []
+        tags << "idc:#{Helper.idc_name}"
+        tags << "env:#{Helper.idc_env}"
+        tags << "app:#{Helper.app_name}"
+        tags << "gem:#{gem_info[0]}"
+        tags << "version:#{gem_info[1]}"
+        tags
+      end
+
+      def exception_metric_tags(exception, opts)
+        tags = []
+        tags << "idc:#{Helper.idc_name}"
+        tags << "env:#{Helper.idc_env}"
+        tags << "app:#{Helper.app_name}"
+        tags << "name:#{exception.class.name}"
+        tags << "at:#{opts.fetch(:at, 'runtime')}"
+        tags << "src:#{opts.fetch(:src, 'app')}"
+        tags
+      end
+    end
+
+    module GemProcessor
+      include Namespace
+
+      def process
+        load_gem_spec.each do |g| 
+          collect_gem_gauge(g)
+        end
+      end
+
+      def load_gem_spec
+        Gem.loaded_specs.inject({}) {
+          |m, (n,s)| m.merge(n => s.version)
+        }
+      end
+
+      def collect_gem_gauge(named_gem,gauge=1, rate=1.0)
+        @collector.gauge(metric_name('gauge'), gauge, sample_rate:rate, tags: gem_metric_tags(named_gem))
+      end
+    end
+
+    module ExceptionProcessor
+      include Namespace
+
+      def process
+        return yield unless Helper.exception_tracable?
+
+        begin
+          exception = yield
+        ensure
+          finish(exception)
+        end
+        return exception
+      end
+
+      def finish(exception)
+        collect_exception_count(exception, @opts)
+      end
+
+      def collect_exception_count(exception, opts, count=1, rate=1.0)
+        @collector.increment(metric_name('count'), count, sample_rate: rate, tags: exception_metric_tags(exception,opts))
+      end
+    end
+
+    PROCESSOR = {
+      'gem' => GemProcessor,
+      'exception' => ExceptionProcessor
+    }
+
+    def self.check_type(type)
+      raise InstrumentNameError.new "Wrong request process type #{type}!" unless PROCESSOR.has_key?(type)
+    end
+
+    def self.instance(collector, opts)
+      type = opts[:type]
+      check_type(type)
+      @instance = new(collector, opts)
+      @instance.extend PROCESSOR[type]
+    end
+
+    def initialize(collector, opts)
+      @collector = collector
+      @opts = opts
+      @type = @opts[:type]
+    end
+
+    def metric_name(type)
+      "#{prefix}.#{type}"
+    end
+
+    def prefix
+      "app.state.#{@type}"
+    end
   end
 end
--- spec/unit/metric/instrumentation/app_exception_spec.rb
+++ spec/unit/metric/instrumentation/app_exception_spec.rb
@@ -3,25 +3,52 @@ require 'd13n/metric'
 require 'd13n/metric/instrumentation/app_exception'
 
 describe D13n::Metric::Instrumentation::AppException do
-
-  describe 'before perform' do
-    let(:kls) {D13n::Error}
-
-    it "should not have instrumentation" do
-      expect(kls).not_to respond_to :exception_with_d13n_instrumentation
-    end
-  end
-
   describe "after perform" do
-    before :each do 
-      allow_any_instance_of(D13n::Metric::Instrumentation::AppException).to receive(:enable?).and_return(true)
+    before :each do
+      allow(D13n::Metric::Instrumentation::AppException).to receive(:enable?).and_return(true)
       D13n::Metric::Instrumentation::Conductor.perform!
     end
 
     it "can raise D13n::Error" do
       expect {raise D13n::Error.new('error')}.to raise_error D13n::Error
     end
+
+    describe 'class method' do
+      it "should have instrumentation with d13n" do
+        expect(D13n::Error).to respond_to(:exception_with_d13n_instrumentation)
+      end
+
+      it "should have instrumentation without d13n" do
+        expect(D13n::Error).to respond_to(:exception_without_d13n_instrumentation)
+      end
+
+      it 'should have original exception alias' do
+        expect(D13n::Error.method(:exception_without_d13n_instrumentation).original_name).to be_eql(:exception)
+      end
+
+      it 'should have new exception alias' do
+        expect(D13n::Error.method(:exception).original_name).to be_eql(:exception_with_d13n_instrumentation)
+      end
+    end
+
+    describe 'instance methods' do
+      let(:instance) {D13n::Error.new}
+
+      it "should have instrumentation with d13n" do
+        expect(instance).to respond_to(:exception_with_d13n_instrumentation)
+      end
+
+      it "should have instrumentation without d13n" do
+        expect(instance).to respond_to(:exception_without_d13n_instrumentation)
+      end
+
+      it 'should have original exception alias' do
+        expect(instance.method(:exception_without_d13n_instrumentation).original_name).to be_eql(:exception)
+      end
+
+      it 'should have new exception alias' do
+        expect(instance.method(:exception).original_name).to be_eql(:exception_with_d13n_instrumentation)
+      end
+    end
   end
-
-
 end
