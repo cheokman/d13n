988dd969b90b7faf380712ae7079bddc8452c06a
Ben Wu <ben.wu@laxino.com>
2018-04-10 18:27:46 +0800

    add camelize method for string

--- lib/d13n/ext/string.rb
+++ lib/d13n/ext/string.rb
@@ -1,4 +1,12 @@
 class String
+  # By default, +camelize+ converts strings to UpperCamelCase.
+  #
+  #   'active_record'.camelize                # => "ActiveRecord"
+  def camelize
+    string = self
+    string.split('_').map{ |e| e.capitalize }.join
+  end
+  
   def underscore
     self.gsub(/::/, '/').
     gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
