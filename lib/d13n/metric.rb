2ac6616b4bcaaf60676e849e8fa317f08cc991bf
Ben Wu <ben.wu@laxino.com>
2018-04-10 18:28:17 +0800

    add http clients in require list

--- lib/d13n/metric.rb
+++ lib/d13n/metric.rb
@@ -1,5 +1,8 @@
 module D13n::Metric
   class MetricError < D13n::Error; end
 end
-
+require 'd13n/metric/http_clients'
 require 'd13n/metric/manager'
+module D13n::Metric
+  class MetricError < D13n::Error;end
+end
