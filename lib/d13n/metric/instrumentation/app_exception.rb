fb3e1676f6171d4fbbe7230713c484fb0997ae9f
Ben Wu <ben.wu@laxino.com>
2018-04-10 17:31:23 +0800

    refactor app exception and test cases

--- lib/d13n/metric/instrumentation/app_exception.rb
+++ lib/d13n/metric/instrumentation/app_exception.rb
@@ -1,5 +1,38 @@
 module D13n::Metric::Instrumentation
   module AppException
+    def self.included(descendance)
+      descendance.include(InstanceMethods)
+      descendance.extend(ClassMethods)
+    end
+
+    module ClassMethods
+      def metric_error_inherated?
+        !!(self < D13n::Metric::MetricError)
+      end
+
+      def exception_with_d13n_instrumentation(*args)
+        return exception_without_d13n_instrumentation(*args) unless metric_error_inherated?
+
+        manager = D13n::Metric::Manager.instance
+        metric = manager.metric(:app_state)
+        
+        if metric.nil?
+          D13n.logger.info "Null intrumentation metric class and ignore collection"
+          return exception_without_d13n_instrumentation(*args)
+        end
+
+        metric.instance(manager, {type: 'exception', at: 'runtime', src: 'app'}).process do
+          exception_without_d13n_instrumentation(*args)
+        end
+      end
+    end
+
+    module InstanceMethods
+      def exception_with_d13n_instrumentation(*args)
+        self.class.exception_with_d13n_instrumentation(*args)
+      end
+    end
+
     module_function
     def enable?
       D13n.config[:'metric.app.state.exception.enable'] == 'true' || D13n.config[:'metric.app.state.exception.enable'] == true
@@ -24,41 +57,10 @@ D13n::Metric::Instrumentation::Conductor.direct do
 
   performances do
     class D13n::Error
+      include D13n::Metric::AppException
       class << self
-        def exception_with_d13n_instrumentation(*args)
-          return exception_without_d13n_instrumentation(*args) if self < D13n::Metric::MetricError
-
-          manager = D13n::Metric::Manager.instance
-          metric = manager.metric(:app_state)
-          
-          if metric.nil?
-            D13n.logger.info "Null intrumentation metric class and ignore collection"
-            return exception_without_d13n_instrumentation(*args)
-          end
-
-          metric.instance(manager, {type: 'exception', at: 'runtime', src: 'app'}).process do
-            exception_without_d13n_instrumentation(*args)
-          end
-        end
-
         alias exception_without_d13n_instrumentation exception
         alias exception exception_with_d13n_instrumentation
-      end
-
-      def exception_with_d13n_instrumentation(*args)
-        return exception_without_d13n_instrumentation(*args) if self.class < D13n::Metric::MetricError
-
-        manager = D13n::Metric::Manager.instance
-        metric = manager.metric(:app_state)
-        
-        if metric.nil?
-          D13n.logger.info "Null intrumentation metric class and ignore collection"
-          return exception_without_d13n_instrumentation(*args)
-        end
-
-        metric.instance(manager, {type: 'exception', at: 'runtime', src: 'app'}).process do
-          exception_without_d13n_instrumentation(*args)
-        end
       end
 
       alias exception_without_d13n_instrumentation exception
--- spec/unit/metric/instrumentation/app_exception_spec.rb
+++ spec/unit/metric/instrumentation/app_exception_spec.rb
@@ -3,51 +3,121 @@ require 'd13n/metric'
 require 'd13n/metric/instrumentation/app_exception'
 
 describe D13n::Metric::Instrumentation::AppException do
-  describe "after perform" do
-    before :each do
-      allow(D13n::Metric::Instrumentation::AppException).to receive(:enable?).and_return(true)
-      D13n::Metric::Instrumentation::Conductor.perform!
-    end
+  class FakeError < D13n::Error
+    include D13n::Metric::Instrumentation::AppException
+  end
 
-    it "can raise D13n::Error" do
-      expect {raise D13n::Error.new('error')}.to raise_error D13n::Error
-    end
+  class MetricTestError < D13n::Metric::MetricError
+    include D13n::Metric::Instrumentation::AppException
+  end
 
-    describe 'class method' do
-      it "should have instrumentation with d13n" do
-        expect(D13n::Error).to respond_to(:exception_with_d13n_instrumentation)
+  describe 'enable?' do
+    describe 'null config' do
+      before(:each) do
+        allow(D13n.config).to receive(:[]).with(:'metric.app.state.exception.enable').and_return(nil)
       end
 
-      it "should have instrumentation without d13n" do
-        expect(D13n::Error).to respond_to(:exception_without_d13n_instrumentation)
-      end
-
-      it 'should have original exception alias' do
-        expect(D13n::Error.method(:exception_without_d13n_instrumentation).original_name).to be_eql(:exception)
-      end
-
-      it 'should have new exception alias' do
-        expect(D13n::Error.method(:exception).original_name).to be_eql(:exception_with_d13n_instrumentation)
+      it 'should return false' do
+        expect(described_class.enable?).to be_falsy 
       end
     end
 
-    describe 'instance methods' do
-      let(:instance) {D13n::Error.new}
-
-      it "should have instrumentation with d13n" do
-        expect(instance).to respond_to(:exception_with_d13n_instrumentation)
+    describe 'true string config' do
+      before(:each) do
+        allow(D13n.config).to receive(:[]).with(:'metric.app.state.exception.enable').and_return('true')
       end
 
-      it "should have instrumentation without d13n" do
-        expect(instance).to respond_to(:exception_without_d13n_instrumentation)
+      it 'should return false' do
+        expect(described_class.enable?).to be_truthy
+      end
+    end
+
+    describe 'true boolean config' do
+      before(:each) do
+        allow(D13n.config).to receive(:[]).with(:'metric.app.state.exception.enable').and_return(true)
       end
 
-      it 'should have original exception alias' do
-        expect(instance.method(:exception_without_d13n_instrumentation).original_name).to be_eql(:exception)
+      it 'should return false' do
+        expect(described_class.enable?).to be_truthy
+      end
+    end
+
+    describe 'included class' do
+      it 'should have class methods' do
+        expect(FakeError).to respond_to(:exception_with_d13n_instrumentation)
       end
 
-      it 'should have new exception alias' do
-        expect(instance.method(:exception).original_name).to be_eql(:exception_with_d13n_instrumentation)
+      it 'should have instance methods' do
+        expect(FakeError.new).to respond_to(:exception_with_d13n_instrumentation)
+      end
+
+      describe 'not metric error' do
+        before(:each) do
+          allow(FakeError).to receive(:exception_without_d13n_instrumentation)
+          allow_any_instance_of(FakeError).to receive(:exception_without_d13n_instrumentation)
+        end
+        
+        it 'should have metric_error_inherated false' do
+          expect(FakeError.metric_error_inherated?).to be_falsy
+        end
+
+        describe 'exception_with_d13n_instrumentation' do
+          it 'should return without instrumentation' do
+            expect(D13n::Metric::Manager).not_to receive(:instance)
+            FakeError.exception_with_d13n_instrumentation
+          end
+
+          it 'should return without instrumentation' do
+            expect(D13n::Metric::Manager).not_to receive(:instance)
+            FakeError.new.exception_with_d13n_instrumentation
+          end
+        end
+      end
+
+      describe 'metric error' do
+        let(:fake_manager) { double("manager") }
+        let(:fake_metric) { double("metric") }
+        let(:fake_processor) { double("processor")}
+        before(:each) do
+          allow(fake_processor).to receive(:process)
+          allow(fake_metric).to receive(:instance) { fake_processor }
+          allow(fake_manager).to receive(:metric) { fake_metric }
+          allow(D13n::Metric::Manager).to receive(:instance) {fake_manager}
+          allow(MetricTestError).to receive(:exception_without_d13n_instrumentation)
+          allow_any_instance_of(MetricTestError).to receive(:exception_without_d13n_instrumentation)
+        end
+
+        it 'should have metric_error_inherated false' do
+          expect(MetricTestError.metric_error_inherated?).to be_truthy
+        end
+
+        describe 'exception_with_d13n_instrumentation' do
+          it 'should return without instrumentation' do
+            expect(D13n::Metric::Manager).to receive(:instance)
+            MetricTestError.exception_with_d13n_instrumentation
+          end
+
+          it 'should return without instrumentation' do
+            expect(D13n::Metric::Manager).to receive(:instance)
+            MetricTestError.new.exception_with_d13n_instrumentation
+          end
+
+          describe 'when metric not found' do
+            before :each do
+              allow(fake_manager).to receive(:metric).and_return(nil)
+            end
+            it 'should call exception_without_d13n_instrumentation' do
+              
+              expect(MetricTestError).to receive(:exception_without_d13n_instrumentation)
+              MetricTestError.new.exception_with_d13n_instrumentation
+            end
+
+            it 'should log information' do
+              expect(D13n.logger).to receive(:info)
+              MetricTestError.new.exception_with_d13n_instrumentation
+            end
+          end
+        end
       end
     end
   end
