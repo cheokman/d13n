a000fb63774af566f036437590123f2f731d5646
Ben Wu <ben.wu@laxino.com>
2018-04-30 21:44:33 +0800

    add controller instrumentation and test cases

---
+++ lib/d13n/metric/instrumentation/controller_instrumentation.rb
@@ -0,0 +1,88 @@
+module D13n::Metric::Instrumentation
+  module ControllerInstrumentation
+    def perform_action_with_d13n_stream(*args, &block)
+      state = D13n::Metric::StreamState.st_get
+      state.request = metric_request(args)
+      return yield unless Helper.http_in_tracable?
+
+      trace_options = args.last.is_a?(Hash) ? args.last : {}
+      category = trace_options[:category] || :action
+      stream_options = create_stream_options(trace_options, category, state)
+
+      begin
+        stream = Stream.start(state category, stream_options)
+
+        begin
+          yield
+        rescue => e
+          D13n::Metric::Manager.notice_error(e)
+          raise
+        end
+      ensure
+        Stream.stop(state)
+      end
+    end
+
+    protected
+
+    def create_stream_options(trace_options, category, state)
+      stream_options = {}
+      stream_options[:request] = trace_options[:request]
+      stream_options[:request] ||= request if respond_to?(:request) rescue nil
+      stream_options[:filtered_params] = trace_options[:params]
+      stream_options[:stream_name] = StreamNamer.name_for(nil, self, category, stream_options)
+    end
+
+    def metric_request(args)
+      opts = args.first
+      if opts.respond_to?(:keys) && opts.respond_to?(:[]) && opts[:request]
+        opts[:request]
+      else self.respond_to?(:request)
+        self.request rescue nil
+      end
+    end
+
+    class StreamNamer
+      def self.name_for(stream, trace_obj, category, options={})
+        "#{prefix_for_category(stream, category)}.#{namespace(trace_obj, options)}"
+      end
+
+      def self.prefix_for_category(stream, category = nil)
+        category ||= (stream && stream.category)
+        case category
+        when :sinatra  then ::D13n::Metric::Stream::SINATRA_PREFIX
+        else "#{category.to_s}"
+        end
+      end
+
+      def self.namespace(traced_obj, options={})
+        return options[:namespace] if options[:namespace]
+
+        kls_name = kls_name(traced_obj, options)
+        if options[:name]
+          if kls_name
+            "#{kls_name}.#{options[:name]}"
+          else
+            options[:name]
+          end
+        elsif traced_obj.respond_to?(:d13n_metric_namespace)
+          traced_obj.d13n_metric_namespace
+        else
+          kls_name
+        end
+      end
+
+      def self.kls_name(trace_obj, options={})
+        return options[:class_name] if options[:class_name]
+
+        if (trace_obj.is_a?(Class) || trace_obj.is_a?(Module))
+          return nil if trace_obj.name.nil?
+          trace_obj.name.underscore
+        else
+          return nil if trace_obj.class.name.nil?
+          trace_obj.class.name.underscore
+        end
+      end
+    end
+  end
+end
---
+++ spec/unit/metric/instrumentation/controller_instrumentation_spec.rb
@@ -0,0 +1,172 @@
+require 'spec_helper'
+require 'd13n/metric'
+require 'd13n/metric/instrumentation/controller_instrumentation'
+
+describe D13n::Metric::Instrumentation::ControllerInstrumentation::StreamNamer do
+  let(:traced_obj_kls) {
+    Class.new {
+      def self.name
+        "TracedKls"
+      end
+    }
+  }
+
+  let(:traced_obj_mod) {
+    Module.new {
+      def self.name
+        "TracedMod"
+      end
+    }
+  }
+
+  let(:not_name_lks) {
+    Class.new {}
+  }
+  before :each do
+    
+  end
+
+  describe "kls_name" do
+    let(:class_name_opt) { {:class_name => "ClassOpt"}}
+    let(:empty_opt) {{}}
+    context "when class_name given in options" do
+      it 'should return class_name in options' do
+        expect(described_class.kls_name(traced_obj_kls, class_name_opt)).to be_eql "ClassOpt"
+      end
+    end
+
+    context "when class_name is not given in options" do
+      context "when traced_obj is a class" do
+        it 'should return class name with underscore' do
+          expect(described_class.kls_name(traced_obj_kls, empty_opt)).to be_eql("traced_kls")
+        end
+      end
+
+      context "when traced_obj is a module" do
+        it 'should return module name with underscore' do
+          expect(described_class.kls_name(traced_obj_mod, empty_opt)).to be_eql("traced_mod")
+        end
+      end
+
+      context "when unnamed traced object" do
+        it 'should return nil' do
+          expect(described_class.kls_name(not_name_lks, empty_opt)).to be_eql nil
+        end
+      end
+    end
+  end
+
+  describe "namespace" do
+    let(:namespace_opt) {{:namespace => "dummy_namespace"}}
+    let(:name_opt) {{:name => "dummy_name"}}
+    before :each do 
+      allow(described_class).to receive(:kls_name).and_return('dummy_class')
+    end
+    context "when namespace given in options" do
+      it 'should return namespace in options' do
+        expect(described_class.namespace(traced_obj_kls, namespace_opt)).to be_eql "dummy_namespace"
+      end
+    end
+
+    context "when namespace is not given in options" do
+      context 'when name given in options' do
+        context 'when nil class name' do
+          before :each do
+            allow(described_class).to receive(:kls_name).and_return(nil)
+          end
+
+          it 'should return name as namespace' do
+            expect(described_class.namespace(traced_obj_kls, name_opt)).to be_eql 'dummy_name'
+          end
+        end
+
+        context 'when return class name' do
+          it 'should return class name dotted with name' do
+            expect(described_class.namespace(traced_obj_kls, name_opt)).to be_eql 'dummy_class.dummy_name'
+          end
+        end
+      end
+
+      context 'when name not given in options' do
+        context "when object have d13n_metric_namespace" do
+          let(:obj_with_method) { Class.new {
+            def self.d13n_metric_namespace
+              "method_namespace"
+            end
+          }}
+
+          it 'should return namespace in d13n_metric_namespace method' do
+            expect(described_class.namespace(obj_with_method)).to be_eql "method_namespace"
+          end
+        end
+
+        context "when object have not d13n_metric_namespace" do
+          it "should return class name as namespace" do
+            expect(described_class.namespace(traced_obj_kls)).to be_eql "dummy_class"
+          end
+        end
+      end
+    end
+  end
+
+  describe 'prefix_for_category' do
+    before :each do
+      @stream = double()
+    end
+
+    context 'when nil category' do
+      context 'when stream assigned category with sinatra' do
+        before :each do
+          allow(@stream).to receive(:category).and_return(:sinatra)
+        end
+
+        it 'should return sinatra prefix' do
+          expect(described_class.prefix_for_category(@stream)).to be_eql ::D13n::Metric::Stream::SINATRA_PREFIX
+        end
+      end
+
+      context 'when stream assigned not recongized category' do
+        before :each do
+          allow(@stream).to receive(:category).and_return(:unknown)
+        end
+        it 'shoild return nil' do
+          expect(described_class.prefix_for_category(@stream)).to be_eql 'unknown'
+        end
+      end
+    end
+
+    context 'when not nil category' do
+      context 'when known category sinatra' do
+        it "should return sinatra" do
+          expect(described_class.prefix_for_category(@stream, :sinatra)).to be_eql ::D13n::Metric::Stream::SINATRA_PREFIX
+        end
+      end
+
+      context "when unknown category" do
+        it "should return unknow" do
+          expect(described_class.prefix_for_category(@stream, :unknown)).to be_eql 'unknown'
+        end
+      end
+    end
+  end
+
+  describe 'name_for' do
+    let(:stream) {double()}
+    let(:obj) {double()}
+    before :each do
+      allow(described_class).to receive(:prefix_for_category).and_return('dummy_prefix')
+      allow(described_class).to receive(:namespace).and_return('dummy_namespace')
+    end
+
+    it 'should join prefix and namespace with dot' do
+      expect(described_class.name_for(stream, obj, :dummy)).to be_eql 'dummy_prefix.dummy_namespace'
+    end
+  end
+end
+
+describe D13n::Metric::Instrumentation::ControllerInstrumentation do
+  before :each do 
+    @dummy_class = Class.new
+    @dummy_class.include(described_class)
+  end
+end
