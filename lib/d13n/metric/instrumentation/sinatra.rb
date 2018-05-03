6d4cb0060c5b84329360af6f1215a31a3f265eb5
Ben Wu <ben.wu@laxino.com>
2018-05-03 16:52:24 +0800

    complete sinatra instrumentation and test case

---
+++ lib/d13n/metric/instrumentation/sinatra.rb
@@ -0,0 +1,168 @@
+require 'd13n/metric/instrumentation/controller_instrumentation'
+require 'd13n/metric/instrumentation/sinatra/stream_namer'
+require 'd13n/rack/metric_middleware'
+D13n::Metric::Instrumentation::Conductor.direct do
+  named :sinatra
+
+  depend_on do
+    D13n.config[:'metric.app.sinatra.enable'] &&
+    defined?(::Sinatra) && defined?(::Sinatra::Base) &&
+    Sinatra::Base.private_method_defined?(:dispatch!) &&
+    Sinatra::Base.private_method_defined?(:process_route) &&
+    Sinatra::Base.private_method_defined?(:route_eval)
+  end
+
+  performances do
+    D13n.logger.info 'Installing Sinatra instrumentation'
+  end
+
+  performances do
+    ::Sinatra::Base.class_eval do
+      include D13n::Metric::Instrumentation::Sinatra
+
+      alias dispatch_without_d13n_instrumentation dispatch!
+      alias dispatch! dispatch_with_d13n_instrumentation
+
+      alias process_route_without_d13n_instrumentation process_route
+      alias process_route process_route_with_d13n_instrumentation
+
+      alias route_eval_without_d13n_instrumentation route_eval
+      alias route_eval route_eval_with_d13n_instrumentation
+    end
+  end
+
+  performances do
+    if Sinatra::Base.respond_to?(:build)
+      require 'd13n/rack/metric_middleware'
+
+      ::Sinatra::Base.class_eval do
+        class << self
+          alias build_without_d13n_instrumentation build
+          alias build build_with_d13n_instrumentation
+
+          alias process_route_without_d13n_instrumentation process_route
+          alias process_route process_route_with_d13n_instrumentation
+        end
+      end
+    else
+      D13n.logger.info("Skipping auto-injection of middleware for Sinatra - require Sinatra 1.2.1+")
+    end
+  end
+
+  performances do
+    if defined?(::SinatraWebsocket) &&
+       defined?(::Sinatra::Request) &&
+       Sinatra::Request.respond_to?(:websocket)
+      ::Sinatra::Base.class_eval do
+        class << self
+          alias websocket_without_d13n_instrumentation websocket
+          alias websocket websocket_with_d13n_instrumentation
+        end
+      end
+    end
+  end
+end
+
+module D13n::Metric::Instrumentation
+  module Sinatra
+    include D13n::Metric::Instrumentation::ControllerInstrumentation
+    
+    def self.included(descendance)
+      descendance.extend(ClassMethods)
+    end
+
+    module ClassMethods
+      def d13n_middlewares
+        middlewares = []
+        if D13n::Rack::MetricMiddleware.enable?
+          middlewares << D13n::Rack::MetricMiddleware
+        end
+        middlewares
+      end
+
+      def websocket_with_d13n_instrumentation(*args, &block)
+        websocket_without_d13n_instrumentation(*args, &block)
+      end
+
+      def build_with_d13n_instrumentation(*args, &block)
+        if auto_middleware_enable?
+          d13n_middlewares.each do |middleware_kls|
+            try_to_use(self, middleware_kls)
+          end
+        end
+
+        build_without_d13n_instrumentation(*args, &block)
+      end
+
+      def process_route_with_d13n_instrumentation(*args, &block)
+        begin
+          env["d13n.last_route"] = args[0]
+        rescue => e
+          D13n.logger.debug("Failed determining last route in Sinatra", e)
+        end
+
+        process_route_without_d13n_instrumentation(*args, &block)
+      end
+
+      def route_eval_with_d13n_instrumentation(*args, &block)
+        begin
+          stream_name = StreamNamer.for_route(env, request)
+          
+          unless stream_name.nil?
+            ::D13n::Metric::Stream.set_default_stream_name("#{self.class.name}.#{stream_name}", :sinatra)
+          end
+        rescue => e
+          D13n.logger.debug("Failed during route_eval to set stream name", e)
+        end
+
+        route_eval_without_d13n_instrumentation(*args, &block)
+      end
+
+      def dispatch_with_d13n_instrumentation
+        request_params = get_request_params
+        name = StreamNamer.initial_stream_name(request)
+        filter_params = get_filter_parames(request_params)
+
+        perform_action_with_d13n_stream(:category => :sinatra,
+                                       :name => name,
+                                       :params => filter_params) do
+          dispatch_and_notice_errors_with_d13n_instrumentation
+        end
+      end
+
+      def dispatch_and_notice_errors_with_d13n_instrumentation
+        dispatch_without_d13n_instrumentation
+      ensure
+        had_error = env.has_key?('sinatra.error')
+        ::D13n::Metric::Manager.notice_error(env['sinatra.error']) if had_error
+      end
+
+      def get_request_params
+        begin
+          @request.params
+        rescue => e
+          D13n.logger.debug("Failed to get params from Rack request.", e)
+        end
+      end
+
+      def get_fileter_parames(request_params)
+        request_params
+      end
+
+      private
+
+      def auto_middleware_enable?
+        D13n.config[:'metric.app.sinatra.auto_middleware.enable']
+      end
+
+      def try_to_use(app, clazz)
+        if app.middleware.nil?
+          D13n.logger.debug("Failed to use middle for middleware missing in app")
+          return nil
+        end
+        has_middleware = app.middleware.any? { |info| info[0] == clazz }
+        app.use(clazz) unless has_middleware
+      end
+    end
+  end
+end
---
+++ spec/unit/metric/instrumentation/sinatra_spec.rb
@@ -0,0 +1,282 @@
+require 'spec_helper'
+require 'd13n/metric'
+require 'd13n/metric/instrumentation/sinatra'
+
+describe D13n::Metric::Instrumentation::Sinatra do
+  class DummyApp
+    include D13n::Metric::Instrumentation::Sinatra
+  end
+
+  let(:dummy_app) { DummyApp }
+  let(:dummy_app_instance) { DummyApp.new }
+  describe '.auto_middleware_enable?' do
+    context 'when metric.app.sintra.auto_middleware.enable false' do
+      
+      before :each do
+        allow(D13n.config).to receive(:[]).with(:'metric.app.sinatra.auto_middleware.enable').and_return(false)
+      end
+
+      it 'should be false' do
+        expect(DummyApp.send(:auto_middleware_enable?)).to be_falsy
+      end
+    end
+
+    context 'when metric.app.sintra.auto_middleware.enable true' do
+      
+      before :each do
+        allow(D13n.config).to receive(:[]).with(:'metric.app.sinatra.auto_middleware.enable').and_return(true)
+      end
+
+      it 'should be false' do
+        expect(DummyApp.send(:auto_middleware_enable?)).to be_truthy
+      end
+    end
+  end
+
+  describe '.try_to_use' do
+    context 'when app no support middleware' do
+      let(:app) { double('app') }
+      let(:clazz) { double('class')}
+      before(:each) do
+        allow(app).to receive(:middleware).and_return(nil)
+      end
+      it 'should return nil' do
+        expect(dummy_app.send(:try_to_use, app, clazz)).to be_nil
+      end
+    end
+
+    context 'when app support middleware' do
+      let(:app) { double('app')}
+      let(:clazz) { double('clazz')}
+      let(:middleware) { [[clazz,'a']]}
+      let(:new_clazz) { double('new_clazz') }
+
+      before(:each) do
+        allow(app).to receive(:middleware).and_return(middleware)
+        allow(app).to receive(:use)
+      end
+
+      context 'when class is in middleware' do
+        it 'should not use' do
+          expect(app).not_to receive(:use)
+          dummy_app.send(:try_to_use, app, clazz)
+        end
+      end
+
+      context 'when new class is not in middleware' do
+        it 'should use' do
+          expect(app).to receive(:use).with(new_clazz)
+          dummy_app.send(:try_to_use, app, new_clazz)
+        end
+      end
+    end
+  end
+
+  describe 'included class' do
+    subject { dummy_app }
+
+    it { is_expected.to respond_to(:websocket_with_d13n_instrumentation)}
+    it { is_expected.to respond_to(:build_with_d13n_instrumentation)}
+    it { is_expected.to respond_to(:process_route_with_d13n_instrumentation)}
+    it { is_expected.to respond_to(:dispatch_with_d13n_instrumentation)}
+    it { is_expected.to respond_to(:dispatch_and_notice_errors_with_d13n_instrumentation)}
+  end
+
+  describe '.d13n_middlewares' do
+    context 'when middleware enable true' do
+      before :each do
+        allow(D13n::Rack::MetricMiddleware).to receive(:enable?).and_return(true)
+      end
+
+      subject { dummy_app.d13n_middlewares }
+      it { is_expected.not_to be_empty }
+      it { is_expected.to include D13n::Rack::MetricMiddleware }
+    end
+
+    context 'when middleware enable false' do
+      before :each do
+        allow(D13n::Rack::MetricMiddleware).to receive(:enable?).and_return(false)
+      end
+
+      subject { dummy_app.d13n_middlewares }
+      it { is_expected.to be_empty }
+    end
+  end
+
+  describe '.build_with_d13n_instrumentation' do
+    let(:middleware) { double() }
+    before :each do
+      allow(dummy_app).to receive(:try_to_use)
+      allow(dummy_app).to receive(:build_without_d13n_instrumentation)
+      allow(dummy_app).to receive(:d13n_middlewares).and_return([middleware])
+    end
+    context 'when auto_middle_ware enable false' do
+      before :each do
+        allow(dummy_app).to receive(:auto_middleware_enable?).and_return(false)
+      end
+
+      it 'should not use d13n middlewares' do
+        expect(dummy_app).not_to receive(:try_to_use)
+        dummy_app.build_with_d13n_instrumentation
+      end
+
+      it 'should call original build' do
+        expect(dummy_app).to receive(:build_without_d13n_instrumentation)
+        dummy_app.build_with_d13n_instrumentation
+      end
+    end
+
+    context 'when auto_middle_ware enable true' do
+      before :each do
+        allow(dummy_app).to receive(:auto_middleware_enable?).and_return(true)
+      end
+
+      it 'should not use d13n middlewares' do
+        expect(dummy_app).to receive(:try_to_use)
+        dummy_app.build_with_d13n_instrumentation
+      end
+
+      it 'should call original build' do
+        expect(dummy_app).to receive(:build_without_d13n_instrumentation)
+        dummy_app.build_with_d13n_instrumentation
+      end
+    end
+  end
+
+  describe '.process_route_with_d13n_instrumentation' do
+    before :each do
+      allow(dummy_app).to receive(:env)
+      allow(dummy_app).to receive(:process_route_without_d13n_instrumentation)
+      RSpec::Mocks.configuration.allow_message_expectations_on_nil = true
+    end
+
+    context 'when exception occurred' do
+      it 'should log the information in logger debug level' do
+        expect(D13n.logger).to receive(:debug)
+        dummy_app.process_route_with_d13n_instrumentation
+      end
+
+      it 'should call original process_route' do
+        expect(dummy_app).to receive(:process_route_without_d13n_instrumentation)
+        dummy_app.process_route_with_d13n_instrumentation
+      end
+    end
+
+    context 'when first argument given' do
+      it 'should assign first argument as last rount in env' do
+        expect(dummy_app.env).to receive(:[]=).with('d13n.last_route', 'dummy_route')
+        dummy_app.process_route_with_d13n_instrumentation('dummy_route')
+      end
+
+      it 'should call original process_route' do
+        expect(dummy_app).to receive(:process_route_without_d13n_instrumentation)
+        dummy_app.process_route_with_d13n_instrumentation
+      end
+    end
+  end
+
+  describe '.route_eval_with_d13n_instrumentation' do
+    before :each do
+      allow(dummy_app).to receive(:route_eval_without_d13n_instrumentation)
+    end
+
+    context 'when stream_name nil' do
+      before :each do
+        allow(D13n::Metric::Instrumentation::Sinatra::StreamNamer).to receive(:for_route).and_return(nil)
+      end
+
+      it 'should not call set_default_stream_name' do
+        expect(::D13n::Metric::Stream).not_to receive(:set_default_stream_name)
+        dummy_app.route_eval_with_d13n_instrumentation
+      end
+
+      it 'should call route_eval_without_d13n_instrumentation' do
+        expect(dummy_app).to receive(:route_eval_without_d13n_instrumentation)
+        dummy_app.route_eval_with_d13n_instrumentation
+      end
+    end
+
+    context 'when stream_name not nil' do
+      before :each do
+        allow(dummy_app).to receive(:env)
+        allow(dummy_app).to receive(:request)
+        allow(D13n::Metric::Instrumentation::Sinatra::StreamNamer).to receive(:for_route).and_return('dummy_name')
+        allow(::D13n::Metric::Stream).to receive(:set_default_stream_name)
+      end
+        
+      it 'should call set_default_stream_name for stream' do
+        expect(::D13n::Metric::Stream).to receive(:set_default_stream_name)
+        dummy_app.route_eval_with_d13n_instrumentation
+      end
+
+      it 'should call route_eval_without_d13n_instrumentation' do
+        expect(dummy_app).to receive(:route_eval_without_d13n_instrumentation)
+        dummy_app.route_eval_with_d13n_instrumentation
+      end
+    end
+
+    context 'when raise error' do
+      before :each do
+        allow(D13n::Metric::Instrumentation::Sinatra::StreamNamer).to receive(:for_route).and_return('dummy_name')
+        allow(::D13n::Metric::Stream).to receive(:set_default_stream_name)
+      end
+
+      it 'should call route_eval_without_d13n_instrumentation' do
+        expect(dummy_app).to receive(:route_eval_without_d13n_instrumentation)
+        dummy_app.route_eval_with_d13n_instrumentation
+      end
+    end
+  end
+
+  describe '#dispatch_with_d13n_instrumentation' do
+    let(:dummy_request_params) { double("dummy_request_params") }
+    let(:dummy_request) { double('dummy_request') }
+    let(:dummy_filter_params) { double('dummy_filter_params')}
+    before :each do
+      allow(dummy_app).to receive(:get_request_params).and_return(dummy_request_params)
+      allow(dummy_app).to receive(:request).and_return(dummy_request)
+      allow(D13n::Metric::Instrumentation::Sinatra::StreamNamer).to receive(:initial_stream_name).and_return('dummy_name')
+      allow(dummy_app).to receive(:get_filter_parames).and_return(dummy_filter_params)
+      allow(dummy_app).to receive(:perform_action_with_d13n_stream)
+    end
+
+    it 'should call perform_action_with_d13n_stream' do
+      expect(dummy_app).to receive(:perform_action_with_d13n_stream)
+      dummy_app.dispatch_with_d13n_instrumentation
+    end
+  end
+
+  describe '#get_request_params' do
+    let(:dummy_request) { double() }
+    context 'when normal' do
+      before :each do
+        allow(dummy_request).to receive(:params).and_return("dummy_params")
+        dummy_app.instance_variable_set(:@request, dummy_request)
+      end
+
+      it 'should return dummy params' do
+        expect(dummy_app.get_request_params).to be_eql "dummy_params"
+      end
+    end
+
+    context 'when error' do
+      before :each do
+        allow(dummy_request).to receive(:params).and_raise
+        dummy_app.instance_variable_set(:@request, dummy_request)
+      end
+
+      it 'should not raise error' do
+        expect {dummy_app.get_request_params}.not_to raise_error
+      end
+
+      it 'should return ni' do
+        expect(dummy_app.get_request_params).to be_nil
+      end
+
+      it 'should logger in debug' do
+        expect(D13n.logger).to receive(:debug)
+        dummy_app.get_request_params
+      end
+    end
+  end
+end
