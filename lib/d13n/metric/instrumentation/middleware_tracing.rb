eae032ae9aeeff931b5e6f4ed6e5c757d5e25b6f
Ben Wu <ben.wu@laxino.com>
2018-05-05 10:17:29 +0800

    add capture result feature and test case

---
+++ lib/d13n/metric/instrumentation/middleware_tracing.rb
@@ -0,0 +1,79 @@
+module D13n::Metric::Instrumentation
+  module MiddlewareTracing
+    STREAM_STARTED_KEY = 'd13n.stream_started'.freeze unless defined?(STREAM_STARTED_KEY)
+
+    def call(env)
+      first_middleware = stream_started(env)
+
+      state = D13n::Metric::StreamState.st_get
+
+      begin
+        Stream.start(state, category, build_stream_options(env, first_middleware))
+       
+        result = (target == self) ? traced_call(env) : target.call(env)
+
+        if first_middleware
+          capture_response_attributes(state, result)
+        end
+
+        result
+      rescue Exception => e
+        D13n.logger.error(e)
+        raise e
+      ensure
+        Stream.stop(state)
+      end
+    end
+
+    def capture_response_code(state, result)
+      if result.is_a?(Array) && state.current_stream
+        state.current_stream.http_response_code = result[0]
+      end
+    end
+
+    CONTENT_TYPE = 'Content-Type'.freeze unless defined?(CONTENT_TYPE)
+    
+    def capture_response_content_type(state, result)
+      if result.is_a?(Array) && state.current_stream
+        _, headers, _ = result
+        state.current_stream.response_content_type = headers[CONTENT_TYPE]
+      end
+    end
+
+    CONTENT_LENGTH = 'Content-Length'.freeze unless defined?(CONTENT_LENGTH)
+
+    def capture_response_content_length(state, result)
+      if result.is_a?(Array) && state.current_stream
+        _, headers, _ = result
+        state.current_stream.response_content_length = headers[CONTENT_LENGTH]
+      end
+    end
+
+    def capture_response_attribute(state, result)
+      capture_response_code(state, result)
+      capture_response_content_type(state, result)
+      capture_response_content_length(state, result)
+    end
+
+    def build_stream_options(env, first_middleware)
+      opts = transaction_options
+      opts = merge_first_middleware_options(opts, env) if first_middleware
+      opts
+    end
+
+    def merge_first_middleware_options(opts, env)
+      opts[:apdex_started_at] = parse_request_timestamp(env)
+
+      opts[:request] = ::Rack::Request.new(env) if defined?(::Rack)
+      opts
+    end
+
+    def parse_request_timestamp(env)
+      Time.now
+    end
+
+    def stream_started(env)
+      env[STREAM_STARTED_KEY] = true unless env[STREAM_STARTED_KEY]
+    end
+  end
+end
---
+++ spec/unit/metric/instrumentation/middleware_tracing_spec.rb
@@ -0,0 +1,141 @@
+require 'spec_helper'
+require 'd13n/metric'
+require 'd13n/metric/instrumentation/middleware_tracing'
+
+describe D13n::Metric::Instrumentation::MiddlewareTracing do
+  class DummyMiddleware
+    include D13n::Metric::Instrumentation::MiddlewareTracing
+  end
+  let(:dm_instance) { DummyMiddleware.new }
+  let(:dummy_stream) { double() }
+  let(:dummy_state) { double() }
+  describe '#stream_started' do
+    let(:started_key) {D13n::Metric::Instrumentation::MiddlewareTracing::STREAM_STARTED_KEY}
+    context 'when not STREAM_STARTED_KEY' do
+      before :each do
+        @dummy_env = {}
+        DummyMiddleware.new.stream_started(@dummy_env)
+      end
+
+      subject { @dummy_env[started_key] }
+      it { is_expected.to be_truthy}
+    end
+
+    context 'when false STREAM_STARTED_KEY' do
+      before :each do
+        @dummy_env = {started_key => false}
+        DummyMiddleware.new.stream_started(@dummy_env)
+      end
+
+      subject { @dummy_env[started_key] }
+      it { is_expected.to be_truthy }
+    end
+
+    context 'when true STREAM_STARTED_KEY' do
+      before :each do
+        @dummy_env = {started_key => true}
+        DummyMiddleware.new.stream_started(@dummy_env)
+      end
+
+      subject { @dummy_env[started_key] }
+      it { is_expected.to be_truthy }
+
+      it 'should return nil' do
+        expect(DummyMiddleware.new.stream_started(@dummy_env)).to be_nil
+      end
+    end
+  end
+
+  describe '#call' do
+   let(:dummy_env) { double() }
+   let(:dummy_state) { double() }
+   
+    before :each do
+      allow(D13n::Metric::StreamState).to receive(:st_get).and_return(dummy_state)
+      allow(D13n::Metric::Stream).to receive(start)
+      allow(D13n::Metric::Stream).to receive(stop)
+    end
+  end
+
+  describe '#capture_response_attribute' do
+    
+    let(:state) { double() }
+    let(:result) { double() }
+    before :each do
+      allow(dm_instance).to receive(:capture_response_code)
+      allow(dm_instance).to receive(:capture_response_content_type)
+      allow(dm_instance).to receive(:capture_response_content_length)
+    end
+
+    it 'should call capture response code' do
+      expect(dm_instance).to receive(:capture_response_code)
+      dm_instance.capture_response_attribute(state, result)
+    end
+
+    it 'should call capture response content length' do
+      expect(dm_instance).to receive(:capture_response_content_length)
+      dm_instance.capture_response_attribute(state, result)
+    end
+
+    it 'should call capture response content type' do
+      expect(dm_instance).to receive(:capture_response_content_type)
+      dm_instance.capture_response_attribute(state, result)
+    end
+  end
+
+  describe '#capture_response_code' do
+    before :each do
+      allow(dummy_stream).to receive(:http_response_code=)
+    end
+
+    context "when result is Array" do
+      let(:dummy_result) { [200, 'headers', 'dummy'] }
+      before(:each) do
+        allow(dummy_state).to receive(:current_stream).and_return(dummy_stream)
+      end
+
+      it 'should assign http_response_code in current stream' do
+        expect(dummy_stream).to receive(:http_response_code=).with(200)
+        dm_instance.capture_response_code(dummy_state, dummy_result)
+      end
+
+      context 'when no current stream' do
+        before :each do
+          allow(dummy_state).to receive(:current_stream).and_return(nil)
+        end
+
+        it 'should not assign http_response_code in current stream' do
+          expect(dummy_stream).not_to receive(:http_response_code=)
+          dm_instance.capture_response_code(dummy_state, dummy_result)
+        end
+      end
+    end
+
+    context 'when result is no Array' do
+      let(:dummy_result) { {} }
+      before(:each) do
+        allow(dummy_state).to receive(:current_stream).and_return(dummy_stream)
+      end
+
+      it 'should not assign http_response_code in current stream' do
+        expect(dummy_stream).not_to receive(:http_response_code=)
+        dm_instance.capture_response_code(dummy_state, dummy_result)
+      end
+    end
+  end
+
+  describe '#capture_response_content_type' do
+    let(:dummy_headers) { {} } 
+    let(:dummy_result) { [200, 'headers', 'dummy'] }
+    before :each do
+      allow(dummy_headers).to receive(:[]).with(D13n::Metric::Instrumentation::MiddlewareTracing::CONTENT_TYPE)
+      allow(dummy_stream).to receive(:response_content_type=)
+      allow(dummy_state).to receive(:current_stream).and_return(dummy_stream)
+    end
+
+    it 'should assign response_content_type' do
+      expect(dummy_stream).to receive(:response_content_type=)
+      dm_instance.capture_response_content_type(dummy_state, dummy_result)
+    end
+  end
+end
