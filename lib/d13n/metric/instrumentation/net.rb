e70bae342703817319fcd6a1b213fb6527abf223
Ben Wu <ben.wu@laxino.com>
2018-05-15 16:23:51 +0800

    update net instrumentation naming

--- lib/d13n/metric/instrumentation/net.rb
+++ lib/d13n/metric/instrumentation/net.rb
@@ -13,24 +13,24 @@ D13n::Metric::Instrumentation::Conductor.direct do
 
   performances do
     class Net::HTTP
-      def request_with_axle_instrumentation(request, *args, &block)
-        return request_without_axle_instrumentation(request, *args, &block) unless started?
+      def request_with_d13n_instrumentation(request, *args, &block)
+        return request_without_d13n_instrumentation(request, *args, &block) unless started?
         wrapped_request = D13n::Metric::HTTPClients::NetHTTPClientRequest.new(self, request)
         manager = D13n::Metric::Manager.instance
         metric = manager.metric(:app_http)
         
         if metric.nil?
           D13n.logger.info "Null intrumentation metric class and ignore collection"
-          return request_without_axle_instrumentation(request, *args, &block) 
+          return request_without_d13n_instrumentation(request, *args, &block) 
         end
 
         metric.instance(manager, direction: 'out').process(wrapped_request) do
-          request_without_axle_instrumentation(request, *args, &block)
+          request_without_d13n_instrumentation(request, *args, &block)
         end
       end
 
-      alias request_without_axle_instrumentation request
-      alias request request_with_axle_instrumentation
+      alias request_without_d13n_instrumentation request
+      alias request request_with_d13n_instrumentation
     end
   end
 end
