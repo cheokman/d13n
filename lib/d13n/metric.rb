863ea4bda0bc5fe330f768e1533e67ab00a6476f
Ben Wu <ben.wu@laxino.com>
2018-05-30 22:56:58 +0800

    add necessary source in require list

--- lib/d13n/metric.rb
+++ lib/d13n/metric.rb
@@ -1,8 +1,11 @@
 module D13n::Metric
   class MetricError < D13n::Error; end
 end
+require 'd13n/metric/stream'
+require 'd13n/metric/stream_state'
 require 'd13n/metric/http_clients'
 require 'd13n/metric/manager'
+
 module D13n::Metric
   class MetricError < D13n::Error;end
 end
